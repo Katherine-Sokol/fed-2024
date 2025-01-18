2fe37a46e31c7fcb2229623f791262170c1f808d
Katherine-Sokol <katherine.kortyak@gmail.com>
2025-01-18 16:24:34 +0200

    hw2

---
+++ JavaScript/02/main.js
@@ -0,0 +1,28 @@
+// ДЗ 2: Математичні операції
+
+// Зробіть розрахунки і виведіть в консоль ось такі математичні операції
+let result1 = (5 + 3) * 2 - 7;
+console.log(`(5 + 3) * 2 - 7 result is ${result1}`);
+
+let result2 = (10 / 2) ** 2;
+console.log(`(10 / 2) ** 2 result is ${result2}`);
+
+let result3 = 2 * (4 + 3) - 8 / 2;
+console.log(`2 * (4 + 3) - (8 / 2) result is ${result3}`);
+
+let result4 = (15 % 4) + 5;
+console.log(`(15 % 4) + 5 result is ${result4}`);
+
+let result5 = 3 - 2 ** 4;
+console.log(`3 - (2 ** 4) result is ${result5}`);
+
+// Також свторіть 2 змінні і самостійно виведіть результат операцій +,-,*,/,%,**
+let firstValue = 5;
+let secondValue = 7;
+console.log(`+: ${firstValue + secondValue}`);
+console.log(`-: ${firstValue - secondValue}`);
+console.log(`*: ${firstValue * secondValue}`);
+console.log(`/: ${firstValue / secondValue}`);
+console.log(`%: ${firstValue % secondValue}`);
+console.log(`**: ${firstValue ** secondValue}`);
+
