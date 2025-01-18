a1fdc8da7e628bccd4c191d13140122a4c5238e3
Katherine-Sokol <katherine.kortyak@gmail.com>
2025-01-18 16:24:27 +0200

    hw1

---
+++ JavaScript/01/main.js
@@ -0,0 +1,13 @@
+// ДЗ 1: Оголошення змінних
+
+// Оголосіть дві змінні: userAgeValue з числовим значенням та userNameNickname з значенням текстовим.
+let userAgeValue = 20;
+let userNameNickname = "Adam Smith";
+console.log(userAgeValue);
+console.log(userNameNickname);
+
+// Змініть значення userAgeValue так задайте нове значення userNameNickname
+userAgeValue = 21;
+userNameNickname = "Adam";
+console.log(userAgeValue);
+console.log(userNameNickname);
